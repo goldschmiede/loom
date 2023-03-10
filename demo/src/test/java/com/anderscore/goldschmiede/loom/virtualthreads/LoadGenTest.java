3a109d4425d97c6cb816e896034541d06b33a405
anderscore-hjhessmann <hans.joerg.hessmann@anderscore.com>
2023-03-10 13:26:37 +0100

    Diagramme ergaenzt

---
+++ demo/src/test/java/com/anderscore/goldschmiede/loom/virtualthreads/LoadGenTest.java
@@ -0,0 +1,32 @@
+package com.anderscore.goldschmiede.loom.virtualthreads;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class LoadGenTest {
+
+    @Test
+    void testThreads() throws InterruptedException {
+        int count = 1_000_000;
+        var countDownLatch = new CountDownLatch(count);
+        long ts = System.nanoTime();
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < count; i++) {
+                executorService.submit(this::work);
+            }
+            countDownLatch.await();
+        }
+    }
+
+    private void work() {
+        BigInteger sum = BigInteger.ZERO;
+        for (long i = 1; i <= 10_000_000_000L; i++) {
+            sum = sum.add(BigInteger.valueOf(i));
+        }
+    }
+
+}
